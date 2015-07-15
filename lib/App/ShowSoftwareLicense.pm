842aed1d21535f1cf0105465028aad6780144db1
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-07-15 19:41:58 +0700

    Hackety hack

--- lib/App/ShowSoftwareLicense.pm
+++ lib/App/ShowSoftwareLicense.pm
@@ -9,6 +9,24 @@ use warnings;
 
 our %SPEC;
 
+$SPEC{show_software_license} = {
+    v => 1.1,
+    summary => 'Show a software license',
+    args => {
+        license => {
+            summary => 'License name',
+            schema  => 'str*',
+            req => 1,
+            pos => 0,
+            'x.schema.entity' => 'software_license',
+        },
+    },
+};
+sub show_software_license {
+    my %args = @_;
+
+    [200];
+}
 
 1;
 # ABSTRACT:
