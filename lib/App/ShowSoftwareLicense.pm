0981d5eecd74a01365eff04565d0bb20be93d747
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-07-15 10:45:22 +0700

    Initial commit (not yet implemented)

---
+++ lib/App/ShowSoftwareLicense.pm
@@ -0,0 +1,24 @@
+package App::ShowSoftwareLicense;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+
+1;
+# ABSTRACT:
+
+=head1 SEE ALSO
+
+L<Software::License>
+
+L<App::ListSoftwareLicenses>
+
+L<App::Software::License>
+
+=cut
